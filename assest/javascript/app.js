96194da4c30e702803cc57972dab53bd17258caa
Charles Ribeiro <chp333@aol.com>
2018-10-13 14:07:57 -0400

    created weather_html and hurricane_js files

--- assest/javascript/app.js
+++ assest/javascript/app.js
@@ -1,92 +1,98 @@
-//Javascript for checklist.html
-var myNodelist = document.getElementsByTagName("LI");
-for(var i = 0; i < myNodelist.length; i++){
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
+$(function () {
 
-var close = document.getElementsByClassName("close");
-for(var i = 0; i < close.length; i++){
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
+   var state = '';
+   var gasType = '';
+   var zip = '';
+   var limit = 5;
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev){
-    if(ev.target.tagName === 'LI'){
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+   function submitReady() {
+       if (gasType != '' && (state != '' || zip != '')) {
+           $("#stationsSubmit").attr('disabled', false);
+       } else {
+           $("#stationsSubmit").attr('disabled', true);
+       }
+   }
 
-function newElement(){
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue === ''){
-        console.log("Write something");
-    } else {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
+   $("#selectState").on("click", function () {
+       state = $("#selectState").val();
+       submitReady();
+   })
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+   $("#selectGasType").on("click", function () {
+       gasType = $("#selectGasType").val();
+       submitReady();
+   })
 
-    for(i = 0; i < close.length; i++){
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
-//end javascript for checklist.html
+   $("#inputZip").change("value", function () {
+       zip = $("#inputZip").val();
+       submitReady();
+   })
 
-//API for Hurricane
+   $("#selectLimit").on("click", function () {
+       limit = $("#selectLimit").val();
+   })
 
-// Adding click event listen listener to all buttons
-$(document).on("click", "#track", function() {
-    // Grabbing and storing the data from Hurricane tracking
+   $("#stationsSubmit").on("click", function (event) {
+       event.preventDefault();
 
- 
-  const searchType = 'random';
+       if (state != '') {
+           var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=" + gasType + "&state=" + state + "&limit=" + limit + "&api_key=gHMrq4MMgbBpXOPWPO6Cf6OqYK80OomuANhCPXMP&format=JSON"
+       }
 
-    // Constructing a queryURL using the animal name
-    var queryURL = "https://api.aerisapi.com/tropicalcyclones/closest?p=chanhassen,mn&radius=1300mi&filter=all&limit=1&format=json&client_id=5MQ1RvB0ZRonDrmxEfyPq&client_secret=tvg7OeULoGZ8zMJhKD6MimPUerXrY2gli9EjtMT4";
-      console.log(queryURL);
+       if (zip != '') {
+           var queryURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=" + gasType + "&zip=" + zip + "&limit=" + limit + "&api_key=gHMrq4MMgbBpXOPWPO6Cf6OqYK80OomuANhCPXMP&format=JSON"
+       }
 
-    // Performing an AJAX request with the queryURL
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
+       // Creates AJAX call for stations button being clicked
+       $.ajax({
+           url: queryURL,
+           method: "GET"
+       }).then(function (response) {
+           $("#dataStations").text("");
+           for (var i = 0; i < response.fuel_stations.length; i++) {
 
-    // After data comes back from the request
-    .then(function(response) {
-        console.log(queryURL);
+               var rowStation = $("<tr>");
+               rowStation.addClass("stationsData");
+               rowStation.attr("data-lat", response.fuel_stations[i].latitude);
+               rowStation.attr("data-long", response.fuel_stations[i].longitude);
+               rowStation.attr("data-address", response.fuel_stations[i].street_address + ', ' + response.fuel_stations[i].city + ' ' + response.fuel_stations[i].state + ', ' + response.fuel_stations[i].zip);
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
+               var colName = $("<td>");
+               colName.append(response.fuel_stations[i].station_name);
 
-          // Creating and storing a div tag
-          var hcane = $("<div class='line1'>");
+               var colAddress = $("<td>");
+               colAddress.append(response.fuel_stations[i].street_address + ', ' + response.fuel_stations[i].city + ' ' + response.fuel_stations[i].state + ', ' + response.fuel_stations[i].zip);
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $(".line1").text(results);
-          
-            
+               var colPhone = $("<td>");
+               colPhone.append(response.fuel_stations[i].station_phone);
 
-          
-          
-      });
-  });
-  // end hurricane Tracker API
+               var colHours = $("<td>");
+               colHours.append(response.fuel_stations[i].access_days_time);
+
+               var colMap = $("<td id='map'>");
+               colMap.append("<i class='fas fa-map-marked-alt'></i>");
+               colMap.attr("data-toggle", "modal");
+               colMap.attr("data-target", "#modalMapStation");
+
+               rowStation.append(colName)
+                   .append(colAddress)
+                   .append(colPhone)
+                   .append(colHours)
+                   .append(colMap)
+
+               $("#dataStations").append(rowStation);
+
+               console.log(response.fuel_stations[i])
+           }
+       })
+   })
+
+   $(document).on("click", "#map", function () {
+       var lat = $(this).parent().attr("data-lat")
+       var long = $(this).parent().attr("data-long")
+       $("#stationAddress").text($(this).parent().attr("data-address"))
+
+       console.log(lat + "  " + long)
+   })
+
+})
