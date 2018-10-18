c174e7dddaa46870cda9a64c75172ff748bba4f3
Roger Perez <rperezga@gmail.com>
2018-10-18 12:18:02 -0400

    Working on login for the checklist

---
+++ javascript/checklist.js
@@ -0,0 +1,46 @@
+$(function(){
+
+    var config = {
+        apiKey: "AIzaSyAtjgM-9GPkeCofL3SXRw6uCedoDQDuINg",
+        authDomain: "hurricaneready-e1c88.firebaseapp.com",
+        databaseURL: "https://hurricaneready-e1c88.firebaseio.com",
+        projectId: "hurricaneready-e1c88",
+        storageBucket: "hurricaneready-e1c88.appspot.com",
+        messagingSenderId: "1012618278075"
+    };
+    firebase.initializeApp(config);
+
+    const db = firebase.firestore();
+    
+    var logged = "false";
+
+    if(logged == "false"){
+        $("#login").attr("hidden", true);
+        $("#nologin").attr("hidden", false);
+    }else{
+        $("#login").attr("hidden", false);
+        $("#nologin").attr("hidden", true);
+    }
+
+    $("#googleLogin").on("click", function(){
+        console.log('Tengo que ver si estoy login o no')
+        provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider)
+            .then(result => {
+                user = result.user;
+                console.log(user);
+                // localStorage.setItem("logged", true);
+                // localStorage.setItem("user", user.displayName);
+
+                // $("#toLogin").attr("hidden", true);
+                // $("#toLogout").attr("hidden", false);
+                // $("#name").text("Hello: " + user.displayName);
+                // $("#newTrain").attr("hidden", false);
+
+                // $("#trainLogin").attr("hidden", false);
+                // $("#trainLogout").attr("hidden", true);
+            })
+            .catch(console.log);
+    });
+
+})
